fbdc72055734149a9c2f2d0724fe88f445836c80
anzinius <anzinius@gmail.com>
2020-11-30 01:59:30 +0900

    Make function generate a result file

--- HTTP_Response.py
+++ HTTP_Response.py
@@ -1,12 +1,24 @@
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
 
-url = "https://github.com/Anzinius"
-try:
-    response = urlopen(url)
-    print(response.status)
-except HTTPError as e:
-    error = e.read()
-    code = e.getcode()
-    print(code)
+code = str()
+f = open("http_status_code.txt", "w")
 
+url = ["https://github.com/Anzinius", "https://github.com/Anzinius/Network", "https://github.com/Anzinius/Networking_Repository"]
+
+def readStatusCode(url):
+    try:
+        response = urlopen(url)
+        code = response.status
+    except HTTPError as e:
+        error = e.read()
+        code = e
+
+def writeStatusCode(code):
+    f.write(code)
+
+for u in url:
+    readStatusCode(u)
+    writeStatusCode(code)
+
+f.close()
