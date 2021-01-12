cb54497c13b649b2343e16afa588960f9ed03e20
anzinius <anzinius@gmail.com>
2021-01-12 22:20:26 +0900

    POST request

---
+++ 2.Packet_Replay/HTTP_Post.py
@@ -0,0 +1,18 @@
+import httplib
+import urllib
+
+conn = httplib.HTTPConnection("github.com/anzinius")
+
+params = "test data"
+headers = {"Connection" : "Keep-Alive",
+    "Host" : "192.168.0.0",
+    "User-Agent" : ""
+}
+conn.request("POST", "/post", params, headers)
+response = conn.getresponse()
+data = response.read()
+print response.status
+print response.reason
+print data
+
+conn.close()
