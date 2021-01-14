4847ab1248cd8153165fbeaee7bcf963e5558b49
anzinius <anzinius@gmail.com>
2021-01-14 14:38:02 +0900

    GET request with a specific port

--- 2.Packet_Replay/HTTP_Get.py
+++ 2.Packet_Replay/HTTP_Get.py
@@ -2,6 +2,8 @@ import httplib
 import urllib
 
 conn = httplib.HTTPConnection("github.com/anzinius")
+#conn = httplib.HTTPConnection("github.com/anzinius", 80)
+#conn = httplib.HTTPConnection("github.com/anzinius:80")
 
 #conn.request("GET", "/ip")
 conn.request("GET", "/get")
