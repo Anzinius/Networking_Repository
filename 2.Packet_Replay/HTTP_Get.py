9db0e561466cb0dff678745a92456686592f4ae3
anzinius <anzinius@gmail.com>
2021-01-16 00:10:58 +0900

    GET requests with a specific port (and fix typo)

--- 2.Packet_Replay/HTTP_Get.py
+++ 2.Packet_Replay/HTTP_Get.py
@@ -10,9 +10,9 @@ conn.request("GET", "/get")
 response = conn.getresponse()
 data1 = response.read()
 data2 = response.read().decode(“utf-8”)
-print response.status
-print response.reason
-print data1
-print data2
+print(response.status)
+print(response.reason)
+print(data1)
+print(data2)
 
 conn.close()
